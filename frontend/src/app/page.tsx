74ecfeee57a8dd9df6d5e6eb1eded0fb27374621
ruperceniza <cenizaruper13@gmail.com>
2025-08-06 17:10:11 +0800

    frontend: next.js and tailwind setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flec justify-center items-center min-h-screen bg-black text-white text-4xl font-bold">
+      Portfolio mfers
+    </main>
+  )
+}
