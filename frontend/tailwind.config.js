333cde1a563ec5e9f9153e68ba41684d1530036d
ruperceniza <cenizaruper13@gmail.com>
2025-08-13 22:34:53 +0800

    feat: custom windows95-styled colors and fonts on tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        win95Gray: '#C0C0C0',
+        win95Dark: '#808080',
+        win95Light: '#FFFFFF',
+        win95Blue: '#000080',
+      },
+      fontFamily: {
+        win95: ['"MS Sans Serif"', 'Tahoma', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
