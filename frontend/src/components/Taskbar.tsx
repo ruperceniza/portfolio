16c142f42965fc211fe6a7475ab4c9548484d2cc
ruperceniza <cenizaruper13@gmail.com>
2025-08-22 15:19:27 +0800

    fix: change start button icon

--- frontend/src/components/Taskbar.tsx
+++ frontend/src/components/Taskbar.tsx
@@ -59,7 +59,7 @@ const Taskbar: React.FC<TaskbarProps> = ({
             onClick={toggleMenu}
             className="flex items-center gap-2 h-6 px-3 bg-[#C0C0C0] border border-gray-500 shadow-[inset_-1px_-1px_#fff,inset_1px_1px_#808080] hover:brightness-105"
           >
-            <img src="/icons/start-icon.jpg" alt="Start" className="w-5 h-5" />
+            <img src="/icons/start-icon.png" alt="Start" className="w-5 h-5" />
             <span className="font-sans text-sm font-bold text-black">Start</span>
           </button>
 
